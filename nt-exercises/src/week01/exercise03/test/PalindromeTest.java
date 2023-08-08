a7689f46b5ca3d7ee8930596455dbc7e5c2769e8
PFonsecaFV <pfonsecafv@gmail.com>
2023-08-08 13:40:01 -0300

    ✨ Exercício 03 - Crie um método que receba como parâmetro um texto do tipo String e retorne se este texto é palíndromo.

---
+++ nt-exercises/src/week01/exercise03/test/PalindromeTest.java
@@ -0,0 +1,12 @@
+package week01.exercise03.test;
+
+import week01.exercise03.classes.Palindrome;
+
+public class PalindromeTest {
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+
+        String word = "ARARA";
+        palindrome.print(word);
+    }
+}
