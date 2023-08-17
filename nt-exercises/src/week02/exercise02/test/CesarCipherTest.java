57bc092cac525b7cb7ebf92096d17d2ed23fd48e
PFonsecaFV <pfonsecafv@gmail.com>
2023-08-17 12:57:46 -0300

    ✨ Exercício 02 - Implemente a Cifra de César para criptografar e descriptografar uma mensagem

---
+++ nt-exercises/src/week02/exercise02/test/CesarCipherTest.java
@@ -0,0 +1,13 @@
+package week02.exercise02.test;
+
+import week02.exercise02.classes.CesarCipher;
+
+public class CesarCipherTest {
+    public static void main(String[] args) {
+        CesarCipher cesarCipher = new CesarCipher();
+        // Exemplo: Java é uma linguagem incrível
+        // Deslocamento: 10
+
+        cesarCipher.display();
+    }
+}
