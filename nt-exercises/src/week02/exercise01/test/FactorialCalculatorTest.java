4eb2d03aa4d6dc93dae3d92fa94b4fa32c93efe9
PFonsecaFV <pfonsecafv@gmail.com>
2023-08-17 11:11:38 -0300

    ✨ Exercício 01 - Implemente um método que receba um número e imprima o resultado do fatorial

---
+++ nt-exercises/src/week02/exercise01/test/FactorialCalculatorTest.java
@@ -0,0 +1,12 @@
+package week02.exercise01.test;
+
+import week02.exercise01.classes.FactorialCalculator;
+
+public class FactorialCalculatorTest {
+    public static void main(String[] args) {
+        FactorialCalculator factorialCalculator = new FactorialCalculator();
+        double number = 8;
+
+        factorialCalculator.print(number);
+    }
+}
